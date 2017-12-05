dc2f385442d4d9fb57fe48796893d05f8776f2b5
Sam Mussell <smussell@apple.com>
2017-12-04 21:20:26 -0800

    day 4

--- day4.js
+++ day4.js
@@ -10,3 +10,27 @@ puzzle.split('\n')
     })
     return valid ? a + 1 : a
   }, 0)
+
+
+// part 2
+
+const R = require('ramda')
+
+const letterCount = s => s.split('')
+  .reduce((a, c) => Object.assign(a, {[c]: a[c] ? a[c] + 1 : 1}), {})
+
+puzzle2.split('\n')
+  .reduce((a, c) => {
+    let seen = []
+    let valid = true
+    const splited = c.split(' ')
+    for (let i = 0; i < splited.length; i++) {
+      const letters = letterCount(splited[i]);
+      if (seen.find(d => R.equals(letters, d))) {
+        valid = false;
+        break;
+      }
+      seen.push(letters)
+    }
+    return valid ? a + 1 : a
+  }, 0);
