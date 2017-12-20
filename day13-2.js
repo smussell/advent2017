aa017afced570bfd9f540657934bec1dd8f5f7dc
sam <sam@sammussell.com>
2017-12-19 17:25:02 -0800

    13 part 2

--- day13-2.js
+++ day13-2.js
@@ -1,62 +1,89 @@
-function getIn() {  
-  return `
-  0: 3
-  1: 2
-  4: 4
-  6: 4`.trim().split('\n')
+  
+const firewalls = `
+0: 3
+1: 2
+2: 6
+4: 4
+6: 4
+8: 8
+10: 6
+12: 8
+14: 5
+16: 6
+18: 8
+20: 8
+22: 12
+24: 6
+26: 9
+28: 8
+30: 12
+32: 12
+34: 17
+36: 12
+38: 8
+40: 12
+42: 12
+44: 10
+46: 12
+48: 12
+50: 12
+52: 14
+54: 14
+56: 10
+58: 14
+60: 12
+62: 14
+64: 14
+66: 14
+68: 14
+70: 14
+72: 14
+74: 14
+76: 14
+86: 14
+94: 20
+96: 18`.trim().split('\n')
     .map(d => d.split(': '))
     .reduce((a ,c) => ({[parseInt(c[0], 10)]: {
-        range: parseInt(c[1], 10), down: true, pos: 0, depth: parseInt(c[0], 10)
+        range: parseInt(c[1], 10), depth: parseInt(c[0], 10)
       }, 
       ...a
     }), {})
+
+const keys = Object.keys(firewalls);
+const indexMap = keys.reduce((a, c, i) => ({[c]: i, ...a}), {});
+let state = keys.map(d => ([0, true]));
+const max = Math.max.apply(Math, keys);
+
+function findPos(t, range) {
+  const i = Math.floor(t /(range - 1));
+  const d = t % (range - 1);
+  return i % 2 === 0 ? 0 + d : (range - 1) - d;  
 }
-
-const input = getIn();
-const keys = Object.keys(input);
-const max = Math.max.apply(Math, keys)
-
-let states = []
-
-for(let i = 0; i < 10000; i++) {
-  states.push({...input})
-}
-
-
 
 function run(start) {
-  
-  let me = -1;
-  let total = 0;
-  let time = 0;
+  let me = 0;
+  let time = start;
+  let caught = false;
   
   while (me <= max) {
-    if(time >= start) me++;
-    if(input[me]) {
-      input[me].pos
-      if(input[me].pos === 0) {
-        total += (input[me].range * input[me].depth);
+    if(firewalls[me]) {
+      const pos = findPos(time, firewalls[me].range);
+      if(pos === 0) {
+        caught = true;
       }
     }
-    states.push(Object.entries(input).map(d => d[1]));
-    Object.keys(input).forEach(d => {
-      const item = input[d];
-      let pos = item.down ? item.pos + 1 : item.pos - 1;
-      let down = pos == 0 || pos == item.range-1 ? !item.down : item.down;
-      input[d] = {...input[d], down, pos}
-    })
     time++;
+    me++;
   }
-  return total;
+  return caught;
 }
-run(0)
 
-// for(let i = 0; i < 100; i++) {
-//   let tot = run(i);
-//   if(tot == 0) {
-//     console.log('done', i)
-//     break;
-//   }
-// }
-
+for(let i = 10000; i <= 4000000; i++) {
+  let caught = run(i);
+  if(!caught) {
+    console.log('done', i)
+    break;
+  }
+}
 
